56231ad18a0ed01caf56f73fefc02b7109b9c398
kirtibhole <93419463+kirtibhole@users.noreply.github.com>
2021-11-25 12:21:08 +0530

    Add files via upload

---
+++ Python/Assignment_7/Assign7_1.py
@@ -0,0 +1,24 @@
+class BookStore:
+    NoOfBooks = 0
+    def __init__(self,name,auther):
+        self.name=name
+        self.author=auther
+        BookStore.NoOfBooks+=1
+
+
+    def Display(self):
+        print("book is:{0} \n auther is:{1} \n no of books:{2}".format(self.name,self.author,self.NoOfBooks))
+
+
+def main():
+
+    Obj1 = BookStore('LinuxSystemProgramming', 'RobertLove')
+    Obj1.Display()  # Linux System Programming by Robert Love. No of books : 1
+    Obj2 = BookStore('C Programming', 'Dennis Ritchie')
+    Obj2.Display()  ## C Programming by Dennis Ritchie. No of books : 2 
+   
+
+
+
+if __name__=="__main__":
+    main()
